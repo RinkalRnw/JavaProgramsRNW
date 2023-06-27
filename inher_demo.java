8b3c015ffd63ed6b2328837db89d6751da4fa98c
RinkalRnw <117502014+RinkalRnw@users.noreply.github.com>
2023-06-27 09:34:28 +0530

    inheritance demo..

---
+++ inher_demo.java
@@ -0,0 +1,15 @@
+class abc{
+    void getName(){
+        System.out.println("Hello");
+    }
+}
+class xyz extends abc{
+    void getXyzName(){
+        
+    }
+}
+public class inher_demo {
+    public static void main(String[] args) {
+        
+    }
+}
