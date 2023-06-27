1f4532a5de62532ca0a6084ed274b059f8295030
RinkalRnw <117502014+RinkalRnw@users.noreply.github.com>
2023-06-27 09:46:24 +0530

    INheritance

--- inher_demo.java
+++ inher_demo.java
@@ -1,15 +1,23 @@
-class abc{
-    void getName(){
-        System.out.println("Hello");
+abstract class abc1{
+    abstract void run();
+}
+class wxyz extends abc1{
+    void run(){
+        System.out.println("run wxyz method called..");
     }
 }
-class xyz extends abc{
-    void getXyzName(){
-        
+class pqrZ extends abc1{
+    void run(){
+        System.out.println("run pqrZ method called..");
     }
-}
+}  
+
 public class inher_demo {
     public static void main(String[] args) {
-        
+        abc1 obj;
+        obj = new wxyz();
+        obj.run();
+        obj = new pqrZ();
+        obj.run();
     }
 }
